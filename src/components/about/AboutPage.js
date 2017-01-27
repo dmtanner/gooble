d9b650722e57e7f640c17245db75d83f2ef2c369
Marcus Tanner <kritiosboy@gmail.com>
2017-01-27 21:50:45 +0000

    added simple routing and a few pages with corresponding react components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is an app that will eventually run Gooble</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
