a75916932737fa0d87fd385c8bb7d20fc7ee6db0
Marcus Tanner <kritiosboy@gmail.com>
2017-01-30 20:44:06 +0000

    added redux components (store, actions, reducers)

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,13 @@
+import * as types from '../actions/actionTypes';
+
+export default function courseReducer(state = [], action) {
+  switch(action.type) {
+    case 'CREATE_COURSE':
+      return [...state,
+        Object.assign({}, action.course)
+      ];
+
+    default:
+      return state;
+  }
+}
