516fa76fb92d7be5a815b76e4e4dc0400f0f6d8e
Marcus Tanner <kritiosboy@gmail.com>
2017-02-07 02:46:01 +0000

    use compression in dev server

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -3,12 +3,15 @@ import webpack from 'webpack';
 import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
+import compression from 'compression';
 
 /* eslint-disable no-console */
 
 const port = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
+
+app.use(compression());
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
